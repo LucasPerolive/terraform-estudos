b1d43682e71abdd865ef4d902f38932d73f26cd6
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2024-06-06 10:23:28 -0300

    Update bucket.tf
    Adiciona comentário no código.

--- criacao-s3-website/bucket.tf
+++ criacao-s3-website/bucket.tf
@@ -1,9 +1,11 @@
-#cria um bucket público para hospedagem de site estético
+# Cria um bucket, configura ele para ser público e hospedar site estático.
 
+# Cria um bucket
 resource "aws_s3_bucket" "exemplo" {
   bucket = "bucket-name-terraform-site"
 }
 
+# Configura o bucket para ser público
 resource "aws_s3_bucket_public_access_block" "exemplo" {
   bucket = aws_s3_bucket.exemplo.id
 
@@ -13,11 +15,13 @@ resource "aws_s3_bucket_public_access_block" "exemplo" {
   restrict_public_buckets = false
 }
 
+# Integra policy no S3
 resource "aws_s3_bucket_policy" "exemplo" {
   bucket = aws_s3_bucket.exemplo.id
   policy = data.aws_iam_policy_document.exemplo.json
 }
 
+# Criação da policy
 data "aws_iam_policy_document" "exemplo" {
   depends_on = [ 
     aws_s3_bucket.exemplo,
@@ -41,6 +45,7 @@ data "aws_iam_policy_document" "exemplo" {
   }
 }
 
+# Configura o WebSite para ser acessar o index.html
 resource "aws_s3_bucket_website_configuration" "exemplo" {
   bucket = aws_s3_bucket.exemplo.id
 
@@ -49,6 +54,7 @@ resource "aws_s3_bucket_website_configuration" "exemplo" {
   }
 }
 
+# Faz upload do site para o bucket
 resource "aws_s3_object" "exemplo" {
   bucket = aws_s3_bucket.exemplo.id
   key    = "index.html" 
