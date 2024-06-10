f1e02c760f064a750a52f6b241cf2c492838894e
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2024-06-10 02:09:24 -0300

    Update variaveis.tf

--- criacao-ec2-com-variaveis/variaveis.tf
+++ criacao-ec2-com-variaveis/variaveis.tf
@@ -24,8 +24,8 @@ locals {
         sudo apt update
         sudo apt-get install apache2 -y
         sudo rm /var/www/html/index.html
-        sudo git clone https://github.com/LucasPerolive/Barbearia-Alura.git
-        sudo mv ./Barbearia-Alura/* /var/www/html/
+        sudo git clone https://github.com/LucasPerolive/exemplo-web.git
+        sudo mv ./pagina/* /var/www/html/
         systemctl restart apache2
     EOF
 }
