8108691b5e48acbcb30f8753efd17e6d0f058597
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2024-06-24 13:46:16 -0300

    Update network.tf

--- criacao-com-dois-providers/network.tf
+++ criacao-com-dois-providers/network.tf
@@ -1,1 +1,9 @@
+resource "aws_vpc" "vpc-us" {
+  cidr_block = "192.168.1.0/24"
+  provider = aws.america-norte
+}
 
+resource "aws_vpc" "vpc-sa" {
+  cidr_block = "10.0.0.0/24"
+  provider = aws.america-sul
+}
